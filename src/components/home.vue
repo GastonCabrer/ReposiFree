c2f983942d94dd5de570b9f30ea2ff892b7ab7ec
GastonCabrer <gaston.cabrera2020@twk.cl>
2020-10-08 01:27:16 -0300

    version3

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,20 @@
+<style>
+.menu {
+
+  background-image: url(../assets/back1.jpg);
+  color: white;
+  border: 2px white;
+  margin: 0px;
+  padding: 2px;
+}
+</style>
 <template>
-<div>
-  <p>
-    Repositorio libre
-  </p>
-</div>
+
+  <div class="menu">
+    <h1>Paisajes</h1>
+    <p>Referencias paisajes gratuitos</p>
+  </div>
+
 </template>
 
 <script>
