d735eed47d5c01039708d27f529d216b7cc17dbb
GastonCabrer <gaston.cabrera2020@twk.cl>
2020-10-08 15:29:34 -0300

    version_0.3

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
+
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <home></home>
-    <tabla></tabla>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import home from './components/home.vue'
-import tabla from './components/tablas.vue'
+
+
 export default {
   name: 'App',
   components: {
-    HelloWorld
-    ,home, tabla,
+    home,
   }
 }
 </script>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,22 +1,63 @@
 <style>
 .menu {
 
-  background-image: url(../assets/back1.jpg);
-  color: white;
+  color: black;
   border: 2px white;
-  margin: 0px;
+  margin: 1px;
   padding: 2px;
+
+}
+
+nav a{
+  text-decoration:none;
+  color:inherit;
+  font-size:14px;
+}
+nav li{
+  display:inline-block;
+  width:10%;
+  padding:5px 10px;
+  background-color:#000;
+  border:1px solid #000;
+  color:#fff;
+}
+nav li:hover{
+  background-color:#fff;
+  color:#000;
+}
+#nav li{
+  float:left;
+  width:10%;
+  padding:5px 10px;
+  background-color:#000;
+  border:10px solid #000;
+  text-align:center;
+  color:#fff;
 }
 </style>
 <template>
 
   <div class="menu">
-    <h1>Paisajes</h1>
-    <p>Referencias paisajes gratuitos</p>
+    <h1>ReposiFree</h1>
+    <p>repositorio gratuito para estudiantes</p>
+    <nav>
+      <ul>
+        <li><a href=”#”>Inicio</a></li>
+        <li><a href=”#”>Anatomia y Perspectiva</a></li>
+        <li><a href=”#”>Recursos Adobe</a></li>
+        <li><a href=”#”>FreeSound</a></li>
+        <li><a href=”#”>Contacto</a></li>
+      </ul>
+    </nav>
+    <body>
+    <h1>Imagen de prueba </h1>
+    <img src="../img-ejemplo/Feliz jueges 3.png" alt="Italian Trulli">
+    </body>
   </div>
-
 </template>
-
+<body>
+<h1>HOLA </h1>
+</body>
 <script>
 export default {
 name: "home"
